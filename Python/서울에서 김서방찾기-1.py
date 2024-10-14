2e4869b44bdec82dfdc1c2090f27fe0a3081abde
hyeeunjeong <hyeun9126@naver.com>
2024-10-14 13:19:36 +0900

    241014 프로그래머스

---
+++ Python/서울에서 김서방찾기-1.py
@@ -0,0 +1,8 @@
+seoul = ["Jane", "Kim"]
+def solution(seoul):
+    answer = ''
+    for i in seoul:
+        if i == "Kim":
+            answer +='김서방은 {}에 있다'.format(seoul.index(i))
+    return answer
+solution(seoul)
